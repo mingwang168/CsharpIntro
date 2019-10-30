4fe8307444c318dc837426ee968097fc4b3f4089
mingwang168 <mingwang0904@gmail.com>
2019-10-30 10:57:04 -0700

    basic variable examples

--- CsharpIntro/Program.cs
+++ CsharpIntro/Program.cs
@@ -4,9 +4,21 @@ namespace CsharpIntro
 {
     class Program
     {
+        // private const string MESSAGE = "Hello World";
+        public string MyName = "Phil";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message="Hello World";
+            int myAge = 39;
+            byte myByte = 7;
+            double myDouble = 10.456;
+            decimal myDecimal = 10.456m;
+            float myFloat = 10.456f;
+            bool myBool = false;
+            DateTime currentDate = DateTime.UtcNow;
+            Console.WriteLine(myBool.ToString()+"is the value of myBool!");
+            Console.WriteLine(currentDate);
+            Console.ReadLine();
         }
     }
 }
